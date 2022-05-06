82243be41491eb19c178f52c2ee2a6c1155bba6b
unknown <lavanyanaidu1999@gmail.com>
2022-05-06 22:09:01 +0530

    script to create resource group

---
+++ main/variable.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  type=string
+  description = "Name of the resource group"
+  default = "techslate-rg"
+}
+
+variable "location" {
+  type=string
+  description = "Select the location"
+  default = "UK South"
+}
